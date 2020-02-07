1d91a89d30c6aef9ba80e5d72f3c4853cb4ed34f
Alessia Amitrano <alessia.amitrano@imagination.com>
2020-02-07 09:39:27 +0000

    game item

---
+++ src/components/GameItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- BootStrap Card -->
+  <div class="card" :class="{'selected': isSelected}" style="width: 12rem;" v-if="gameItem">
+    <img :src="imgSrc" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{gameItem.name}}</h5>
+      <p class="card-text">{{gameItem.year}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    gameItem: {
+      type: Object,
+      default: function() {
+        return {
+          id: Number,
+          console_id: Number,
+          name: String,
+          year: String,
+          image: String,
+          description: String
+        };
+      }
+    },
+    isSelected: false
+  },
+  data() {
+    return {
+      imgSrc: require("../assets/imgs/" + this.gameItem.image)
+    };
+  },
+  methods: {}
+};
+</script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="main-container">
-    <div class="consoles">
-      <div v-for="(gConsole, index) in gameConsoles" :key="index">
-        <app-console
-          :gConsole="gConsole"
-          :selectConsoleFn="selectConsole"
-          :isSelected="selectedConsole && gConsole.id === selectedConsole.id"
-        ></app-console>
+    <div class="flex-container">
+      <div class="consoles">
+        <div v-for="(gConsole, index) in gameConsoles" :key="index">
+          <app-console
+            :gConsole="gConsole"
+            :selectConsoleFn="selectConsole"
+            :isSelected="selectedConsole && gConsole.id === selectedConsole.id"
+          ></app-console>
+        </div>
+      </div>
+      <div class="games" v-if="selectedConsoleGames.length > 0">
+        <div v-for="(game, index) in selectedConsoleGames" :key="index">
+          <app-game :gameItem="game"></app-game>
+        </div>
       </div>
     </div>
   </div>
@@ -14,22 +21,30 @@
 
 <script>
 import GameConsole from "./GameConsole.vue";
+import GameItem from "./GameItem.vue";
 import { eventBus } from "./../main";
 export default {
   data() {
     return {
       gameConsoles: [],
-      games: eventBus.games,
+      selectedConsoleGames: [],
       selectedConsole: null
     };
   },
   components: {
-    "app-console": GameConsole
+    "app-console": GameConsole,
+    "app-game": GameItem
   },
   methods: {
     selectConsole(selConsole) {
       this.selectedConsole = selConsole;
       console.log(this.selectedConsole);
+      this.selectedConsoleGames = this.loadGames(this.selectedConsole.id);
+    },
+    loadGames(consoleId) {
+      return eventBus.games.filter(game => {
+        return game.console_id === consoleId;
+      });
     }
   },
   mounted() {
@@ -39,13 +54,21 @@ export default {
 </script>
 
 <style scoped>
-.consoles {
-  width: 100%;
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+
+.consoles,
+.games {
+  width: 20%;
   height: 850px;
   display: flex;
   flex-direction: column;
   flex-wrap: nowrap;
   justify-content: space-between;
+  align-items: center;
   padding: 2rem;
   overflow: scroll;
 }
